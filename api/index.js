e16a9dc9391a14c73155b37f839ce66db495925f
irfankhann0 <pikhan552@gmail.com>
2026-02-22 14:22:29 +0530

    vercel fixed paths

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
+require("dotenv").config();
 const express = require("express");
 const path = require("path");
-require("dotenv").config();
 const connectDB = require("../config/db");
 const authRoutes = require("../routes/auth");
 const authMiddleware = require("../middleware/authMiddleware");
