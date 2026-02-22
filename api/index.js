b1551acb6d8a7e547cf50b6a95367b3788a2d631
irfankhann0 <pikhan552@gmail.com>
2026-02-22 15:58:36 +0530

    railway server fix

--- api/index.js
+++ api/index.js
@@ -35,10 +35,9 @@ app.use("/uploads", express.static("/tmp/uploads"));
 app.get("/u/:username", (req, res) => {
   res.sendFile(path.join(__dirname, "public/profile.html"));
 });
-if (process.env.NODE_ENV !== "production") {
 const PORT = process.env.PORT || 5000;
-  app.listen(PORT, () => {
-    console.log("Server running on port " + PORT);
-  });
-}
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
 module.exports = app;
