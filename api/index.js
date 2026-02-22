15682605829ba030f7488812e9c93286ea8455d1
irfankhann0 <pikhan552@gmail.com>
2026-02-22 15:07:05 +0530

    local server fix

--- api/index.js
+++ api/index.js
@@ -35,4 +35,10 @@ app.use("/uploads", express.static("uploads"));
 app.get("/u/:username", (req, res) => {
   res.sendFile(path.join(__dirname, "public/profile.html"));
 });
+if (process.env.NODE_ENV !== "production") {
+  const PORT = 5000;
+  app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
+  });
+}
 module.exports = app;
